389b2450cccc54394bcb37bc3c327cb8ee486f45
Confiks <confiks@scriptbase.org>
2019-05-06 14:34:58 +0200

    chore: Convert indentation consistently to 2 spaces

--- src/components/app.js
+++ src/components/app.js
@@ -5,18 +5,18 @@ import Home from '../pages/home';
 
 export default class App extends Component {
 
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
+  handleRoute = e => {
+    this.currentUrl = e.url;
+  };
 
-	render() {
-		return (
-			<div id="app">
-				<Router onChange={this.handleRoute}>
-					<Home path="/" language="nl" />
-					<Home path="/en" language="en" />
-				</Router>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="app">
+        <Router onChange={this.handleRoute}>
+          <Home path="/" language="nl" />
+          <Home path="/en" language="en" />
+        </Router>
+      </div>
+    );
+  }
 }
